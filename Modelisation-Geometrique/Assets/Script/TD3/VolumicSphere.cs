32660d51be67cad58db3c215a9615bca481aa579
KaTSukiFireStar <78344193+KatSukiFireStar@users.noreply.github.com>
2024-10-10 13:46:15 +0200

    add intersection operator

--- Modelisation-Geometrique/Assets/Script/TD3/VolumicSphere.cs
+++ Modelisation-Geometrique/Assets/Script/TD3/VolumicSphere.cs
@@ -6,16 +6,20 @@ public class VolumicSphere : MonoBehaviour
 {
     [SerializeField] private GameObject cube;
 
+    [Header("Octree information")]
     [SerializeField] private bool adaptiveOctree;
-    
-    [SerializeField] private List<int> radius = new();
-
-    [SerializeField] private List<Vector3> centers = new();
-
     [SerializeField] private int precision;
     
+    [Header("Sphere information")]
+    [SerializeField] private List<int> radius = new();
+    [SerializeField] private List<Vector3> centers = new();
+    
+    [Header("Operator")]
+    [SerializeField] private bool doIntersection;
+
     private void Start()
     {
+        List<Octree> octrees = new();
         if (radius.Count != centers.Count)
         {
             Debug.LogError("Radius and Centers must have the same number of elements.");
@@ -24,6 +28,7 @@ public class VolumicSphere : MonoBehaviour
         for (int i = 0; i < centers.Count; i++)
         {
             Octree octree = new Octree();
+            octrees.Add(octree);
             if (adaptiveOctree)
             {
                 octree.CreateAdaptiveOctree(centers[i], radius[i] * 2, precision, centers[i], radius[i]);
@@ -32,13 +37,51 @@ public class VolumicSphere : MonoBehaviour
             {
                 octree.CreateRegularOctree(centers[i], radius[i] * 2, precision);
             }
-            
-            FiledOctree(centers[i], radius[i], octree);
+
+            if (!doIntersection)
+            {
+               FiledOctree(centers[i], radius[i], octree);
+            }
+        }
+
+        if (doIntersection)
+        {
+            FiledOctreeIntersection(octrees[0]);
+        }
+    }
+
+    private void FiledOctreeIntersection(Octree octree)
+    {
+        if (octree.isOctreeParent)
+        {
+            foreach (Octree o in octree.Octrees)
+            {
+                FiledOctreeIntersection(o);
+            }
+        }
+        else if (octree.isVoxelParent)
+        {
+            foreach (Voxel v in octree.Voxels)
+            {
+                bool add = true;
+                for (int i = 0; i < centers.Count; i++)
+                {
+                    if (Vector3.Distance(centers[i], v.Center) > radius[i])
+                    {
+                        add = false;
+                    }
+                }
+                if (add)
+                {
+                    cube.transform.localScale = Vector3.one * (v.PointMax - v.PointMin).x;
+                    Instantiate(cube, v.Center, Quaternion.identity, transform);
+                }
+            }
         }
     }
 
 
-    public void FiledOctree(Vector3 center, int radius, Octree octree)
+    private void FiledOctree(Vector3 center, int radius, Octree octree)
     {
         if (octree.isOctreeParent)
         {
