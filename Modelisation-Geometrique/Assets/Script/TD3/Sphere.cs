5e209bf2ea02a741007c4d0bddcfd7d4f8e6e950
KaTSukiFireStar <78344193+KatSukiFireStar@users.noreply.github.com>
2024-10-11 10:44:54 +0200

    I can reformat like i want

--- Modelisation-Geometrique/Assets/Script/TD3/Sphere.cs
+++ Modelisation-Geometrique/Assets/Script/TD3/Sphere.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 [Serializable]
 public class Sphere
 {
-	[SerializeField]
-	private Vector3 position;
-	public Vector3 Position { get => position; set => position = value; }
+    [SerializeField]
+    private Vector3 position;
 
-	[SerializeField]
-	private float radius;
-	public float Radius { get => radius; set => radius = value; }
+    public Vector3 Position
+    {
+        get => position;
+        set => position = value;
+    }
+
+    [SerializeField]
+    private float radius;
+
+    public float Radius
+    {
+        get => radius;
+        set => radius = value;
+    }
 }
--- Modelisation-Geometrique/Assets/Script/TD3/VolumicSphere.cs
+++ Modelisation-Geometrique/Assets/Script/TD3/VolumicSphere.cs
@@ -4,29 +4,36 @@ using UnityEngine;
 
 public class VolumicSphere : MonoBehaviour
 {
-    [SerializeField] private GameObject cube;
+    [SerializeField]
+    private GameObject cube;
 
     [Header("Octree information")]
-    [SerializeField] private bool adaptiveOctree;
-    [SerializeField] private int precision;
-    
+    [SerializeField]
+    private bool adaptiveOctree;
+
+    [SerializeField]
+    private int precision;
+
     [Header("Sphere information")]
-    [SerializeField] private List<Sphere> spheres = new();
-    
+    [SerializeField]
+    private List<Sphere> spheres = new();
+
     [Header("Operator")]
-    [SerializeField] private bool doIntersection;
+    [SerializeField]
+    private bool doIntersection;
 
     private void Start()
     {
         List<Octree> octrees = new();
-        
+
         for (int i = 0; i < spheres.Count; i++)
         {
             Octree octree = new Octree();
             octrees.Add(octree);
             if (adaptiveOctree)
             {
-                octree.CreateAdaptiveOctree(spheres[i].Position, spheres[i].Radius * 2, precision, spheres[i].Position, spheres[i].Radius);
+                octree.CreateAdaptiveOctree(spheres[i].Position, spheres[i].Radius * 2, precision, spheres[i].Position,
+                    spheres[i].Radius);
             }
             else
             {
@@ -35,7 +42,7 @@ public class VolumicSphere : MonoBehaviour
 
             if (!doIntersection)
             {
-               FiledOctree(spheres[i].Position, spheres[i].Radius, octree);
+                FiledOctree(spheres[i].Position, spheres[i].Radius, octree);
             }
         }
 
@@ -66,6 +73,7 @@ public class VolumicSphere : MonoBehaviour
                         add = false;
                     }
                 }
+
                 if (add)
                 {
                     cube.transform.localScale = Vector3.one * (v.PointMax - v.PointMin).x;
